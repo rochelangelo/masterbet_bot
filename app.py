8042d31393a8f68b1966f6de29c83ffdfef41543
RochelAngelo <jrochelangelo@gmail.com>
2025-02-18 22:10:01 -0300

    melhorias e aprimoriações

--- app.py
+++ app.py
@@ -1,22 +1,57 @@
 import requests
 from time import sleep
+from jogos import capturar_jogos
 
 last_update_id = None
+token = '7764658414:AAFgcOnbE0NGLe-58UX8_NH79zNwjtkB7CQ'
+
+def enviar_mensagem(chat_id, texto):
+    requests.get(f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={texto}')
 
 while True:
     sleep(1)
-    token = '7764658414:AAFgcOnbE0NGLe-58UX8_NH79zNwjtkB7CQ'
 
     data = requests.get(f'https://api.telegram.org/bot{token}/getUpdates?timeout=100')
 
     if len(data.json()['result']) > 0:
-        update_id = data.json()['result'][-1]['update_id']
-        if last_update_id != update_id:
-            chat_id = data.json()['result'][-1]['message']['chat']['id']
-            mensagem = data.json()['result'][-1]['message']['text']
+        update = data.json()['result'][-1]  # Última atualização recebida
+        update_id = update['update_id']
 
-            if mensagem in ('selecione os jogos', 'jogos', 'jogos para hoje'):
-                mensagem_aguarde = 'Analisando aguarde, MasterBeteiro!'
-                requests.get(f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={mensagem_aguarde}')
+        if last_update_id != update_id:
+            if 'message' in update and 'text' in update['message']:
+                chat_id = data.json()['result'][-1]['message']['chat']['id']
+                mensagem = data.json()['result'][-1]['message']['text'].lower().strip()  # Converter para minúsculas e remover espaços extras
+
+                comandos_obter_jogos = ('Selecione os jogos', 'Jogos', 'Jogos para hoje', 'selecione os jogos', 'jogos', 'jogos para hoje')
+
+                if mensagem in comandos_obter_jogos:
+                    enviar_mensagem(chat_id, "🔍 Analisando jogos, aguarde um momento...")
+
+                    # Chamar a função para capturar jogos
+                    resultado = capturar_jogos()
+
+                    # Verificar se ocorreu um erro
+                    if "Erro" in resultado:
+                        enviar_mensagem(chat_id, f"⚠️ Ocorreu um erro: {resultado}")
+                    else:
+                        enviar_mensagem(chat_id, "✅ Jogos analisados com sucesso! Enviando arquivo...")
+
+                        # Enviar o arquivo CSV
+                        with open(resultado, 'rb') as arquivo:
+                            requests.post(f'https://api.telegram.org/bot{token}/sendDocument',
+                                        data={'chat_id': chat_id},
+                                        files={'document': arquivo})
+                else:
+                    # Mensagem padrão para instruções
+                    instrucoes = (
+                        "🤖 *Bem-vindo ao MasterBeteiro Bot!* 🎲⚽\n\n"
+                        "Use os seguintes comandos para receber o arquivos com os jogos:\n"
+                        "✅ *'selecione os jogos'/'Selecione os jogos'*\n"
+                        "✅ *'jogos'/'Jogos'*\n"
+                        "✅ *'jogos para hoje'/'Jogos para hoje'*\n\n"
+                        # "❓ Caso tenha dúvidas, basta enviar uma mensagem!"
+                    )
+
+                    enviar_mensagem(chat_id, instrucoes)
 
             last_update_id = update_id
--- jogos.py
+++ jogos.py
@@ -7,116 +7,114 @@ import pandas as pd
 import time
 import os
 
-# Configurar o ChromeDriver
-chrome_driver_path = "./chromedriver-win64/chromedriver.exe"
-service = Service(chrome_driver_path)
-options = webdriver.ChromeOptions()
-options.add_argument("--disable-blink-features=AutomationControlled")
-driver = webdriver.Chrome(service=service, options=options)
-
-# URL do site de apostas
-url = "https://superbet.bet.br/apostas/futebol/hoje"
-driver.get(url)
-
-# Aguardar a página carregar completamente
-wait = WebDriverWait(driver, 10)
-
-# Nome do arquivo CSV
-arquivo_csv = "jogos_selecionados.csv"
-
-# Verificar se o arquivo CSV já existe, senão criar
-try:
-    df_existente = pd.read_csv(arquivo_csv)
-except FileNotFoundError:
-    df_existente = pd.DataFrame(columns=["Horário", "Campeonato", "Time Casa", "Time Fora", "Odd Casa", "Odd Empate", "Odd Fora", "Mercados", "Link_Partida"])
-    df_existente.to_csv(arquivo_csv, index=False, encoding="utf-8")
-
-try:
-    # Fechar pop-up de cookies
+def capturar_jogos():
     try:
-        botao_cookies = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Aceitar todos os cookies')]")))
-        botao_cookies.click()
-        time.sleep(1)
-    except Exception:
-        pass
+        # Configurar o ChromeDriver
+        chrome_driver_path = "./chromedriver-win64/chromedriver.exe"
+        service = Service(chrome_driver_path)
+        options = webdriver.ChromeOptions()
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        driver = webdriver.Chrome(service=service, options=options)
 
-    # Fechar pop-up de múltiplas populares
-    try:
-        botao_fechar_popup = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'close')]")))
-        botao_fechar_popup.click()
-        time.sleep(1)
-    except Exception:
-        pass
+        # URL do site de apostas
+        url = "https://superbet.bet.br/apostas/futebol/hoje"
+        driver.get(url)
 
-    # Clicar no botão +12h
-    botao_12h = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='quick-filter']//button[contains(text(), '+12h')]")))
-    botao_12h.click()
-    time.sleep(2)
+        # Aguardar a página carregar completamente
+        wait = WebDriverWait(driver, 10)
 
-    # Função para rolar a página até o final
-    def scroll_ate_fim():
-        last_height = driver.execute_script("return document.body.scrollHeight")
-        while True:
-            driver.execute_script("window.scrollBy(0, window.innerHeight);")
-            time.sleep(2)
-            new_height = driver.execute_script("return document.body.scrollHeight")
-            if new_height == last_height:
-                break
-            last_height = new_height
+        # Nome do arquivo CSV
+        arquivo_csv = "jogos_selecionados.csv"
 
-    # Função para capturar e salvar os jogos
-    def capturar_e_salvar_dados():
-        elementos = driver.find_elements(By.CLASS_NAME, "group-header__wrapper")
-        novos_dados = []
+        # Verificar se o arquivo CSV já existe, senão criar
+        if not os.path.exists(arquivo_csv):
+            df_existente = pd.DataFrame(columns=["Horário", "Campeonato", "Time Casa", "Time Fora", "Odd Casa", "Odd Empate", "Odd Fora", "Mercados", "URL"])
+            df_existente.to_csv(arquivo_csv, index=False, encoding="utf-8")
 
-        for elemento in elementos:
-            try:
-                titulo = elemento.find_element(By.CLASS_NAME, "group-header__details-title-text").text
-                eventos = elemento.find_elements(By.CLASS_NAME, "event-card")
+        # Fechar pop-ups
+        try:
+            botao_cookies = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Aceitar todos os cookies')]")))
+            botao_cookies.click()
+            time.sleep(1)
+        except Exception:
+            pass
 
-                for evento in eventos:
-                    try:
-                        times = evento.find_elements(By.CLASS_NAME, "event-competitor__name")
-                        odds = evento.find_elements(By.CLASS_NAME, "odd-button__odd-value-new")
-                        horario_elemento = evento.find_element(By.CLASS_NAME, "event-card-label")
-                        mercado_btn = evento.find_element(By.CLASS_NAME, "odd-offer__more-odds-button").text
-                        link_elemento = evento.find_element(By.TAG_NAME, "a")
-                        jogo_url = link_elemento.get_attribute("href")
+        try:
+            botao_fechar_popup = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(@class, 'close')]")))
+            botao_fechar_popup.click()
+            time.sleep(1)
+        except Exception:
+            pass
 
-                        if len(times) == 2 and len(odds) == 3:
-                            mercados = int(mercado_btn.replace("+", "").strip())
-                            if mercados >= 230:
-                                jogo = (
-                                    horario_elemento.text.strip(),
-                                    titulo,
-                                    times[0].text.strip(),
-                                    times[1].text.strip(),
-                                    odds[0].text.strip(),
-                                    odds[1].text.strip(),
-                                    odds[2].text.strip(),
-                                    mercado_btn.strip(),
-                                    jogo_url
-                                )
-                                novos_dados.append(jogo)
-                    except Exception:
-                        continue
-            except Exception:
-                continue
+        # Clicar no botão +12h
+        botao_12h = wait.until(EC.element_to_be_clickable((By.XPATH, "//div[@class='quick-filter']//button[contains(text(), '+12h')]")))
+        botao_12h.click()
+        time.sleep(2)
 
-        if novos_dados:
-            df_novos = pd.DataFrame(novos_dados, columns=["Horário", "Campeonato", "Time Casa", "Time Fora", "Odd Casa", "Odd Empate", "Odd Fora", "Mercados", "Link_Partida"])
-            df_novos.to_csv(arquivo_csv, mode='a', header=not os.path.exists(arquivo_csv), index=False, encoding="utf-8")
+        # Função para rolar a página até o final
+        def scroll_ate_fim():
+            last_height = driver.execute_script("return document.body.scrollHeight")
+            while True:
+                driver.execute_script("window.scrollBy(0, window.innerHeight);")
+                time.sleep(2)
+                new_height = driver.execute_script("return document.body.scrollHeight")
+                if new_height == last_height:
+                    break
+                last_height = new_height
 
-    # Iniciar processo de coleta
-    scroll_ate_fim()
-    capturar_e_salvar_dados()
+        # Função para capturar e salvar os jogos
+        def capturar_e_salvar_dados():
+            elementos = driver.find_elements(By.CLASS_NAME, "group-header__wrapper")
+            novos_dados = []
 
-except Exception as e:
-    print(f"Erro: {e}")
+            for elemento in elementos:
+                try:
+                    titulo = elemento.find_element(By.CLASS_NAME, "group-header__details-title-text").text
+                    eventos = elemento.find_elements(By.CLASS_NAME, "event-card")
 
-finally:
-    driver.quit()
-    df = pd.read_csv(arquivo_csv)
-    df.drop_duplicates(inplace=True)
-    df.to_csv(arquivo_csv, index=False, encoding="utf-8")
-    print("✅ Arquivo limpo e salvo sem duplicatas!")
+                    for evento in eventos:
+                        try:
+                            times = evento.find_elements(By.CLASS_NAME, "event-competitor__name")
+                            odds = evento.find_elements(By.CLASS_NAME, "odd-button__odd-value-new")
+                            horario_elemento = evento.find_element(By.CLASS_NAME, "event-card-label")
+                            mercado_btn = evento.find_element(By.CLASS_NAME, "odd-offer__more-odds-button").text
+                            link_elemento = evento.find_element(By.TAG_NAME, "a")
+                            jogo_url = link_elemento.get_attribute("href")
+
+                            if len(times) == 2 and len(odds) == 3:
+                                mercados = int(mercado_btn.replace("+", "").strip())
+                                if mercados >= 230:
+                                    jogo = (
+                                        horario_elemento.text.strip(),
+                                        titulo,
+                                        times[0].text.strip(),
+                                        times[1].text.strip(),
+                                        odds[0].text.strip(),
+                                        odds[1].text.strip(),
+                                        odds[2].text.strip(),
+                                        mercado_btn.strip(),
+                                        jogo_url
+                                    )
+                                    novos_dados.append(jogo)
+                        except Exception:
+                            continue
+                except Exception:
+                    continue
+
+            if novos_dados:
+                df_novos = pd.DataFrame(novos_dados, columns=["Horário", "Campeonato", "Time Casa", "Time Fora", "Odd Casa", "Odd Empate", "Odd Fora", "Mercados", "URL"])
+                df_novos.to_csv(arquivo_csv, mode='a', header=not os.path.exists(arquivo_csv), index=False, encoding="utf-8")
+
+        # Iniciar processo de coleta
+        scroll_ate_fim()
+        capturar_e_salvar_dados()
+
+        driver.quit()
+        df = pd.read_csv(arquivo_csv)
+        df.drop_duplicates(inplace=True)
+        df.to_csv(arquivo_csv, index=False, encoding="utf-8")
+
+        return arquivo_csv
+
+    except Exception as e:
+        return f"Erro: {e}"
